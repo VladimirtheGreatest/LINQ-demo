d473f75219ae96525e4c4a7f80b056aead78632e
VladimirtheGreatest <vladkodoktorov@gmail.com>
2020-01-25 19:40:15 +0000

    first commit

---
+++ Linqdemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80 && score != 97
+                select score;
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("executed");
+            Console.ReadLine();
+        }
+    }
+}
