27a5a66364915cc57f149b9a696e0fe5e939764e
VladimirtheGreatest <vladkodoktorov@gmail.com>
2020-01-25 19:58:28 +0000

    added query and random number generator to simulate getting records from the database

--- Linqdemo/Program.cs
+++ Linqdemo/Program.cs
@@ -10,21 +10,39 @@ namespace Linqdemo
     {
         static void Main(string[] args)
         {
-            // Specify the data source.
-            int[] scores = new int[] { 97, 92, 81, 60 };
 
-            // Define the query expression.
+            //random number generator
+            int[] randomNum = new int[1000];
+            Random RandomNumber = new Random();
+            List<int> termsList = new List<int>();
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                randomNum[i] = RandomNumber.Next(1, 1000);
+            }
+
+
+            foreach (int j in randomNum)
+            {
+                termsList.Add(j);
+            }
+
+            //termsList.ForEach(i => Console.Write("{0}\t", i));
+
+
+
+            //Define the query expression.
             IEnumerable<int> scoreQuery =
-                from score in scores
-                where score > 80 && score != 97
-                select score;
+                from something in termsList
+                where something > 800 && something < 1000
+                select something;
 
-            // Execute the query.
+            //Execute the query.
             foreach (int i in scoreQuery)
             {
                 Console.Write(i + " ");
             }
-            Console.WriteLine("executed");
             Console.ReadLine();
         }
     }
